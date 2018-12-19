377a1fa18d41bdfe8b02425eb68fd59898048b5e
lahirurajapaksa <lahiru9954@gmail.com>
2018-12-19 21:24:52 +0530

    Original code has been issued comments to simplify it

--- newproject/style.js
+++ newproject/style.js
@@ -1,3 +1,4 @@
+//declare all variables
 var paddleLx;
 var paddleLy;
 var paddleRx;
@@ -32,29 +33,35 @@ function restart() {
 }
 
 function setup() {
+    //creates the canvas
     createCanvas(400, 400);
+    // change the background attribute so that it changes colour as mousex and mousey changes
     background(0);
     restart();
-    textSize(60);
+    // creates the text for the initial page
+    textSize(50);
     fill(255);
     text("PONG", 100, 160);
-    text("MANIA", 120, 240);
+    text("MODIFIED", 120, 240);
     fill(0,0,255);
     text("PONG", 100+3, 160+3);
-    text("MANIA", 120+3, 240+3);
+    text("MODIFIED", 120+3, 240+3);
 }
 
+//begins the game if the mouse is pressed
 function mousePressed() {
     if (gameOn == 0) {
         gameOn = 1;
         ballVx = 5;
     }
+    //if the game has ended and the mouse has been pressed again, the game is restarted
     else {
         restart();
         gameOn = 0;
     }
 }
 
+//main function called for the game to be played
 function update() {
     background(0);
     fill(255, 100);
@@ -62,17 +69,25 @@ function update() {
     text(LScore, 10, 30);
     text(RScore, 360, 30);
 
+    //responsible for moving the user's paddle
     paddleLy = mouseY;
-    ballX += ballVx;
-    ballY += ballVy;
 
+    //
+    ballX=ballX+ballVx;
+    ballY=ballY+ballVy;
+
+    //increments ticker variable
     ++ticker;
 
+    //defines the movement for the right paddle to move autonomously
     paddleRy = int(ballY + 50*sin(sin((ballY + ticker)/30)));
 
+    // if the y coordinate of the ball is out of bounds, its direction is reversed
     if (ballY < 0 || ballY > 400) {
-        ballVy *= -1;
+        ballVy=ballVy*-1
     }
+
+    //rules for handling how the paddles interact with the ball
     else if ((paddleLx - bigWidth < ballX) && (ballX < paddleLx + bigWidth) && (paddleLy - bigHeight < ballY) && (ballY < paddleLy + bigHeight)) {
         ballVy = ((ballY - paddleLy)/float(bigHeight))*4;
         ballVx *= -1;
@@ -83,14 +98,16 @@ function update() {
         ballVx *= -1;
         ballX -= 1;
     }
+    //if player loses
     else if (ballX < -2) {
         ballVx = ballVy = 0;
         textSize(32);
-        text("You lose!", 200, 200);
+        text("GAME OVER", 200, 200);
         ++RScore;
         gameOn = 0;
         restart();
     }
+    //if player wins
     else if (ballX > 402) {
         ballVx = ballVy = 0;
         textSize(32);
@@ -100,7 +117,7 @@ function update() {
         restart();
     }
 }
-
+//draw function (repeats continoulsy throughout the course of the sketch)
 function draw() {
     if (gameOn == 1) {
         update();
